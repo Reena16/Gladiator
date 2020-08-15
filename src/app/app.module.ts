97331d64ba67645b606382e89b1dc084e63ab30d
Stuti Gupta <stutigupta1779@gmail.com>
2020-08-15 11:19:59 +0530

    Login Commit Stuti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    LoginPageComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
