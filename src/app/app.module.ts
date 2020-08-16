fa3058c571f975cf2b7719f1af55484dfb6f5c34
Stuti Gupta <stutigupta1779@gmail.com>
2020-08-16 14:37:17 +0530

    Login updated stuti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,24 +8,14 @@ import { CalculatorComponent } from './calculator/calculator.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { enableProdMode } from '@angular/core'
-import { FusionChartsModule } from 'angular-fusioncharts';
+
 import { FaqPageComponent } from "./faq-page/faq-page.component";
 
 
-// Load FusionCharts
-import * as FusionCharts from 'fusioncharts';
-// Load Charts module
-import * as Charts from 'fusioncharts/fusioncharts.charts';
-// Load themes
-import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+
 import { BankInfoComponent } from './bank-info/bank-info.component';
 
-
-FusionChartsModule.fcRoot(
-  FusionCharts,
-  Charts,
-  FusionTheme
-)
 
 @NgModule({
   declarations: [
@@ -41,7 +31,7 @@ FusionChartsModule.fcRoot(
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    FusionChartsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent],
