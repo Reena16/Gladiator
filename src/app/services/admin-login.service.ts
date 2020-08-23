35fd8e37f57d96a6067d1d0928067cc0eca4715c
Reena16 <Reenabhaira98@gmail.com>
2020-08-23 18:21:26 +0530

    .

--- src/app/services/admin-login.service.ts
+++ src/app/services/admin-login.service.ts
@@ -10,7 +10,7 @@ export class AdminLoginService {
   constructor(private http:HttpClient) { }
 
   loginAdmin(login:AdminLogin):Observable<any>{
-    let url="http://localhost:8180/adminLogin";
+    let url="http://localhost:8188/adminLogin";
     return this.http.post(url,login);
   }
 }
