4a51e662ec5796a0cb2a714744cb8ed9fc3f8167
Stuti Gupta <stutigupta1779@gmail.com>
2020-08-25 11:38:57 +0530

    Update profile done stuti

--- src/app/models/Application.ts
+++ src/app/models/Application.ts
@@ -1,4 +1,45 @@
 import{ Customer } from './customer'
 export class Application{
     customer:Customer;
+    
+	gender:string;
+
+	address:string;
+
+	userPAN:string;
+
+	aadharNumber:string;
+
+	Nationality:string;
+
+	 retirementAge:number;
+
+	organisation:string;
+	
+	 income:number;
+	
+	loanAmount:number;
+
+	 tenure:number;
+	
+	 downPayment:number;
+	
+	 maritialStatus:string;
+	
+	 noOfDependents:string;
+	
+	 depandentMonthlyExpenses;
+	
+	 personlExpenses;
+	
+	 existingLoan;
+	
+	 emiExistingLoan;
+	
+	 maxLoanAmount; //read only field based on above parameters and will be calculated on front end
+	
+	loanStatus:string;
+	
+	appointmentDate:string;
+	
 }
