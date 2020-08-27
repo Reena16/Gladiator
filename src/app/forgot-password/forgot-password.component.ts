d6f849cb7ed2e4ad861fb907ed50e5661ccbde76
Reena16 <reenabhaira98@gmail.com>
2020-08-27 08:54:40 +0530

    .

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import {  ForgotPassword} from "../models/forgotPassword";
+import { ForgotPassword } from "../models/forgotPassword";
 import { LoginserviceService } from "../loginservice.service";
 @Component({
   selector: 'app-forgot-password',
@@ -9,19 +9,21 @@ import { LoginserviceService } from "../loginservice.service";
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  forgot:ForgotPassword=new ForgotPassword();
-  message:string;
-  constructor(private router : Router,private service : LoginserviceService ) { }
+  forgot: ForgotPassword = new ForgotPassword();
+  message: string;
+  constructor(private router: Router, private service: LoginserviceService) { }
 
   ngOnInit(): void {
   }
-
-  forgotPass(){
-    this.service.forgotPassword(this.forgot).subscribe(data=>{
-      if(data.status=='SUCCESS'){
+  forgotPass() {
+    console.log(this.forgot);
+    this.service.forgotPassword(this.forgot).subscribe(data => {
+      this.forgot.email = data;
+      if (data.status == 'SUCCESS') {
         this.message = data.message;
-        this.router.navigate(['/resetPasswordLink']);
-      }else{
+        console.log(this.message);
+        this.router.navigate(['resetPasswordLink']);
+      } else {
         this.message = data.message;
       }
     })
