d51b595c81c677c10b7e3455f9d9568beac8c174
Reena16 <reenabhaira98@gmail.com>
2020-08-27 15:15:35 +0530

    forgotPassword

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,31 +1,115 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { ForgotPassword } from "../models/forgotPassword";
-import { LoginserviceService } from "../loginservice.service";
+import {  ForgotPasswordService} from "../forgot-password.service";
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
-
-  forgot: ForgotPassword = new ForgotPassword();
-  message: string;
-  constructor(private router: Router, private service: LoginserviceService) { }
-
-  ngOnInit(): void {
-  }
-  forgotPass() {
-    console.log(this.forgot);
-    this.service.forgotPassword(this.forgot).subscribe(data => {
-      this.forgot.email = data;
-      if (data.status == 'SUCCESS') {
-        this.message = data.message;
-        console.log(this.message);
-        this.router.navigate(['resetPasswordLink']);
-      } else {
-        this.message = data.message;
+    constructor(private forgotPasswordService : ForgotPasswordService, private router:Router) { }
+    
+      show:boolean=true
+      show1:boolean=false
+      show2:boolean=false
+      show3:boolean=false
+      userOtp:any;
+      userEmail:any;
+      forgotPasswordDetails:ForgotPassword=new ForgotPassword()
+      message:string
+      message1:string
+      message2:string
+      message3:string
+        
+      
+    
+      getOTP()
+      {
+        this.userEmail=this.forgotPasswordDetails.email
+        this.forgotPasswordService.verifyEmail(this.forgotPasswordDetails).subscribe(
+         data=>{
+           if(data.status=='SUCCESS')
+           {
+             this.message=""
+             this.userOtp=data.userOTP
+             this.show=false;
+             this.show1=true;
+             this.show2=false;
+             this.show3=false;
+             this.message=""
+             this.message1=""
+             this.message2=""
+             this.message3=""
+           }
+           else
+           {
+             this.message=data.message
+             this.message1=""
+             this.message2=""
+             this.message3=""  
+            
+           }
+         })
       }
-    })
-  }
-}
+    
+    verifyOTP()
+    {
+      if(this.userOtp==this.forgotPasswordDetails.otp) {
+    
+             this.show=false;
+             this.show1=false;
+             this.show2=true;
+             this.show3=false;
+             this.message=""
+             this.message1=""
+             this.message2=""
+             this.message3=""
+      }
+      else
+      {
+        this.message1="OTP verification failed!"
+        this.message=""
+        this.message2=""
+        this.message3=""
+        
+      }
+    }
+    
+    resetPassword()
+    {
+      if (this.forgotPasswordDetails.newPassword===this.forgotPasswordDetails.newPasswordConfirm) 
+      {
+        this.message2=""
+        this.forgotPasswordService.forgotPassword(this.forgotPasswordDetails).subscribe(
+          data=>{
+          if(data.status=='SUCCESS')
+           {
+          
+             this.show2=false;
+             this.show3=true;
+           }
+           else
+           {
+             this.message2=data.message
+             
+              
+           }
+          }
+        )  
+      }
+      else
+      {
+               this.message2="Password didn't match!"
+               this.message1=""
+               this.message=""
+               this.message3=""
+      }
+    
+    }
+      
+      ngOnInit(): void {
+      }
+      
+    }
+    
